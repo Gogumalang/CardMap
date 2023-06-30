08eab0e5ea9f502555c60102edb59458988acf7f
Gogumalang <22000269@handong.ac.kr>
2023-06-30 21:15:25 +0900

    install NaverMap-Api

--- android/app/src/main/kotlin/com/example/cardmap/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cardmap/MainActivity.kt
@@ -4,3 +4,13 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+}
