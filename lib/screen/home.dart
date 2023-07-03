0a1261e5a0f11cfeb8604f31530c638d6443f328
Yo Han Yoon <hani@Youi-MacBookAir.T-mobile.com>
2023-07-03 14:34:42 +0900

    login function

---
+++ lib/screen/home.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  void signUserout() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserout,
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text("Logged in!"),
+      ),
+    );
+  }
+}
