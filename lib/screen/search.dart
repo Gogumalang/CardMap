955c65959fcf274b3ec03fc702d51fe262d27964
Yo Han Yoon <hani@Youi-MacBookAir.T-mobile.com>
2023-07-25 17:10:59 +0900

    idk

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,4 +1,3 @@
-import 'package:cardmap/screen/home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -98,8 +97,7 @@ class _SearchScreenState extends State<SearchScreen> {
                     _filter.clear();
                     _searchText = "";
                     focusNode.unfocus();
-                    Get.off(const HomePage(),
-                        transition: Transition.noTransition);
+                    Get.back();
                   },
                 ),
               ),
